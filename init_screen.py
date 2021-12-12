a0a6666a32ef17e9020c6247740f13da827d1f02
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-12-12 23:02:54 +0300

    Update init_screen.py

--- init_screen.py
+++ init_screen.py
@@ -3,3 +3,9 @@ from pygame.draw import *
 
 screen = pygame.display.set_mode((1000, 900))
 pygame.display.set_caption("Sea Battle")
+
+pygame.init()
+FPS = 100
+
+pygame.display.update()
+clock = pygame.time.Clock()
