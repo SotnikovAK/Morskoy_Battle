721a5a09b2642fc954dfc38ceaea9a35891d9864
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-12-12 23:00:40 +0300

    Update Operators.py

--- Operators.py
+++ Operators.py
@@ -6,7 +6,7 @@ from _background import *
 
 b_n = Button()
 
-def operator_on_buttons(number, screen_id, event):
+def operator_on_buttons(number, screen_id, event,_old):
     a, b, c = 0, screen_id, 0
     gamemode = -1
     for i in range(number):
@@ -20,19 +20,19 @@ def operator_on_buttons(number, screen_id, event):
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if (b_n.pressure_test(event)):
                 a, b, c = post_pressing_effect[screen_id][i]
-                gamemode = Gamemode[screen_id][i] 
+                gamemode = Gamemode[screen_id][i]
                 if (b == -10):
-                    b = screen_id
+                    b = _old
 
     return a, b, c, gamemode 
 
-def operator_on_screen(screen_id,gamemode, event):
+def operator_on_screen(screen_id,gamemode, event, _old):
     old_screen_id = screen_id
     new_gamemode = 0
 
     flag_quit, screen_id, ship_choice, new_gamemode = operator_on_buttons(
-        len(button_pushed_image[screen_id]), screen_id, event)
-
+        len(button_pushed_image[screen_id]), screen_id, event,_old)
+    
     if (new_gamemode != -1):
         gamemode = new_gamemode
 
