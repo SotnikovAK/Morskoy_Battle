6ea540ad25c8c64afc73a5e80f50f59a9b976164
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-12-21 16:54:33 +0300

    Update handler_for_single_player_screen.py

--- handler_for_single_player_screen.py
+++ handler_for_single_player_screen.py
@@ -95,7 +95,18 @@ def handler_for_attack_ai_in_single_player(flag_quit, screen_id, ship_choice, ga
     this is the mechanism of his attack
     """
     """
-    see the normal player attack documentation and the documentation in the ai file
+    see the normal player attack documentation and the documentation in the ai file and player attack
+    everything will be visible there
+    general documentation for ai sam:
+    
+    * if we destroy the ship, we return to the first stage of shooting at random (stage_attack_sam = 1)
+    * depending on the length of the ship, we change the stage of the attack
+    
+    so destroyers are destroyed at stage 1
+    so cruisers are destroyed in stage 2
+    so aircraft carriers are destroyed in stage 3
+    to destroy battleships you need stages 4,5,6
+    
     """
     if stage_attack_sam == 1:
         if flag_move:
@@ -129,27 +140,6 @@ def handler_for_attack_ai_in_single_player(flag_quit, screen_id, ship_choice, ga
             flag_hit = add.attack_on_ships(x, y, player, 'Sam')
 
         if flag_hit == 0:
-            flag_move = False
-
-        if flag_hit == 1:
-            """
-            there is a break
-            """
-            if not AI.question_about_destroyed_ship():
-                AI.angle_determinant()
-                AI.diagonal_death_zone()
-                stage_attack_sam = 3
-            else:
-                stage_attack_sam = 1
-    if stage_attack_sam == 3:
-        if flag_move:
-            """
-            if oscar can walk, then the player has the right to attack
-            """
-            x, y = AI.third_type_of_attack()
-            flag_hit = add.attack_on_ships(x, y, player, 'Sam')
-
-        if flag_hit == 0:
             """
             player did not hit the ship, so blocked from further attack
             """
@@ -159,12 +149,81 @@ def handler_for_attack_ai_in_single_player(flag_quit, screen_id, ship_choice, ga
             """
             there is a break
             """
-            if AI.question_about_destroyed_ship():
+            AI.diagonal_death_zone()
+            if not AI.question_about_destroyed_ship():
+                AI.angle_determinant()
+                stage_attack_sam = 3
+            else:
                 stage_attack_sam = 1
+
+    if stage_attack_sam == 3:
+        if flag_move:
+            """
+            if oscar can walk, then the player has the right to attack
+            """
+            x, y = AI.third_type_of_attack()
+            if x != -1 and y != -1:
+                flag_hit = add.attack_on_ships(x, y, player, 'Sam')
+                if flag_hit == 0:
+                    """
+                    player did not hit the ship, so blocked from further attack
+                    """
+                    stage_attack_sam = 4
+                    flag_move = False
+
+                elif flag_hit == 1:
+                    AI.angle_determinant()
+                    fl = AI.question_about_destroyed_ship()
+                    if not fl:
+
+                        stage_attack_sam = 5
+
+                    else:
+                        stage_attack_sam = 1
+            else:
+                stage_attack_sam = 4
+
+    if stage_attack_sam == 4:
+        if flag_move:
+            """
+            if oscar can walk, then the player has the right to attack
+            """
+            x, y = AI.fourth_type_of_attack()
+            if x != -1 and y != -1:
+                flag_hit = add.attack_on_ships(x, y, player, 'Sam')
+                if flag_hit == 1:
+                    if not AI.question_about_destroyed_ship():
+
+                        stage_attack_sam = 6
+
+                    else:
+                        stage_attack_sam = 1
+
+    if stage_attack_sam == 5:
+
+        if flag_move:
+            x, y = AI.fifth_type_of_attack()
+            if x != -1 and y != -1:
+                flag_hit = add.attack_on_ships(x, y, player, 'Sam')
+            else:
+                stage_attack_sam = 1
+        if flag_hit == 0:
+            flag_move = False
+
+        elif flag_hit == 1:
+            AI.question_about_destroyed_ship()
+            stage_attack_sam = 1
+
+    if stage_attack_sam == 6:
+        if flag_move:
+            x, y = AI.six_type_attack()
+            flag_hit = add.attack_on_ships(x, y, player, 'Sam')
+        if flag_hit == 1:
+            stage_attack_sam = 1
 
     if flag_hit == 2:
         """
-        access to handling standard buttons
+        victory screen
         """
         screen_id = 8
         static_background(screen_id)
@@ -183,4 +242,3 @@ def handler_for_attack_ai_in_single_player(flag_quit, screen_id, ship_choice, ga
         static_background(screen_id)
 
     return flag_quit, screen_id, ship_choice, game_mode, player, flag_move, event, coord_of_map, stage_attack_sam
-
